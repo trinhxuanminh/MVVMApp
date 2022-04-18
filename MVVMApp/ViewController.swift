17eff84ddf6e89ed191aaa05d3c6f409be95cb9b
Trịnh Xuân Minh <trinhxuanminh2000@gmail.com>
2022-04-18 13:32:56 +0700

    Initial Commit

---
+++ MVVMApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMApp
+//
+//  Created by Trịnh Xuân Minh on 18/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
